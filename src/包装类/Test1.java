e3cd8d3c32e1f2dec7bc4727129adb0645495f3f
yanbo0039 <yanbo0039@yeah.net>
2019-07-11 10:44:10 +0800

    封装类介绍一

---
+++ src/包装类/Test1.java
@@ -0,0 +1,24 @@
+package 包装类;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		/**
+		 * 所有的包装类：
+		 * 1.为了解决基本类型没有对象的问题，所以为8种基本类型各自量身定做了包装类
+		 * 2.包装类一共有8个：
+		 * 3.封装类提供了丰富的数据类型之间转换的方法
+		 */
+		Character c;
+		Boolean b;
+		Float f;
+		Double d;
+		
+		Byte bt;
+		Short s;
+		Integer i;
+		Long ong;
+	}
+
+}
